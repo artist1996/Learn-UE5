ec348b7002267064d3f9ba053168bfe97bfbd707
성민 최 <ilycsm03@gmail.com>
2025-05-27 17:30:47 +0900

    캐릭터 공격 판정

---
+++ Source/ArenaBattle/Character/ABCharacterNonePlayer.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "ABCharacterNonePlayer.h"
+
+AABCharacterNonePlayer::AABCharacterNonePlayer()
+{
+
+}
+
+void AABCharacterNonePlayer::SetDead()
+{
+	Super::SetDead();
+
+	FTimerHandle DeadTimerHandle;
+	GetWorld()->GetTimerManager().SetTimer(DeadTimerHandle, FTimerDelegate::CreateLambda(
+		[&]()
+		{
+			Destroy();
+		}
+	), DeadEventDelayTime, false);
+}
